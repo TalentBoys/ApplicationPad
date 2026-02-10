427f82b0e367ec916224744e95d80c01789cdb42
qjkobe <513967622@qq.com>
2026-02-10 10:26:34 +0800

    Initial Commit

---
+++ ApplicationPad/ApplicationPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationPadApp.swift
+//  ApplicationPad
+//
+//  Created by Jin, Kris on 2026/2/10.
+//
+
+import SwiftUI
+
+@main
+struct ApplicationPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
